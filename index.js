1b69d3ca78c8e3491b0139c684de7e1b2ef785c3
Holly919 <shinydog2010@gmail.com>
2020-02-18 14:28:16 -0500

    Task 1, 2 and questions complete, still working on Task 3

--- index.js
+++ index.js
@@ -39,9 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 
 }
+
+Person.prototype.eat = function(food) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(food);
+  }
+};
+
+Person.prototype.poop = function() {
+  return (this.stomach = []);
+};
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+};
 
 /*
   TASK 2
@@ -57,9 +74,20 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons) {
+  return (this.tank += gallons);
+};
+
+
+
+
 
 /*
   TASK 3
@@ -68,19 +96,30 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+  }
+Baby.prototype = Object.create(Person.prototype);
 
-}
+Baby.prototype.play = function() {
+  return `Playing with $(this.favoriteToy)`;
+};
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global Binding- When a function is in the global scope, or outside of any functions, 'this' will be the whole window Object. 
+  
+  2. Implicit Binding- The most common rule, when you call a function and when it's invoked, look to the left of the . and that's what the 'this' will reference. 
+  
+  3. New Binding- a method is called from a constructor function's prototype.
+
+  4. Explicit Binding- you state explicitly what the 'this' keyword is when called and it's used with functions .call, .apply and .bind.
 */
+
 
 
 ///////// END OF CHALLENGE /////////
